04ad0d67bec56c3ff864c8a55dd4796477555fb6
Andrae Raymond <Araymond12@gmail.com>
2015-04-14 04:45:04 -0500

    Added autofill of values on page load of trip

--- location/trip.php
+++ location/trip.php
@@ -13,37 +13,37 @@ include_once('top.php');
 			
 		<p> <label for="name1">Friend 1</label>
 			Name:
-			<input type="text" name="friend1" size ="15" id="name"> 
+			<input type="text" name="friend1" size ="10" id="name" value="larry"> 
 			Xposition:
-			<input type="text" name="x1" size="2">
+			<input type="text" name="x1" size="2" value="23">
 			Yposition:
-			<input type="text" name="y1" size="2">
+			<input type="text" name="y1" size="2" value="100">
 			Gas price:
-			<input type="text" name="gas1" size="2">
+			<input type="text" name="gas1" size="2" value="3.5">
 			MPG:
 			<input type="text" name="mpg1" size="2" value="25"></p>
 			
 		<p> <label for="name2">Friend 2</label>
 			Name:
-			<input type="text" name="friend2" size ="15" id="name"> 
+			<input type="text" name="friend2" size ="10" id="name" value="Curly"> 
 			Xposition:
-			<input type="text" name="x2" size="2">
+			<input type="text" name="x2" size="2" value="-4">
 			Yposition:
-			<input type="text" name="y2" size="2">
+			<input type="text" name="y2" value="10"size="2">
 			Gas price:
-			<input type="text" name="gas2" size="2">
+			<input type="text" name="gas2" size="2" value="2.6">
 			MPG:
 			<input type="text" name="mpg2" size="2" value="25"></p>
 			
 		<p> <label for="name1">Friend 3</label>
 			Name:
-			<input type="text" name="friend3" size ="15" id="name" > 
+			<input type="text" name="friend3" size ="10" id="name" value="Moe" > 
 			Xposition:
-			<input type="text" name="x3" size="2">
+			<input type="text" name="x3" size="2" value="30">
 			Yposition:
-			<input type="text" name="y3" size="2">
+			<input type="text" name="y3" size="2" value="500">
 			Gas price:
-			<input type="text" name="gas3" size="2">
+			<input type="text" name="gas3" size="2" value="4">
 			MPG:
 			<input type="text" name="mpg3" size="2" value="25"></p>
 			
